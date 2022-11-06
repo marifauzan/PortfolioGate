ab9054bdac001dc1f1e18a126fd53ebfe2b9c185
Mar'i Fauzan <marifauzan20@gmail.com>
2022-11-06 16:09:20 +0700

    Create page not found

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main class="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+      <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
+      <div class="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
+        Page Not Found
+      </div>
+    </main>
   );
 }
 
